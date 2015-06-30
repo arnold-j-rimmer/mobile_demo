cb8a4aec0fa87f23bb310d7e8568a06ad0a91287
Arnold J. Rimmer <djordjekg@gmail.com>
2015-06-30 21:21:48 +0200

    tt

--- client/www/app/shared/mainController.js
+++ client/www/app/shared/mainController.js
@@ -1,33 +1,33 @@
 
 var ngApp = angular.module('ngApp',['ngRoute', 'ui.bootstrap', 'ngTouch']);
-
+/*
 ngApp.config(['$locationProvider', function($locationProvider){
 	$locationProvider.html5Mode({
 		enabled: true,
 		requireBase: false
 	});
 }]);
-
+*/
 ngApp.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: '/app/components/home/view.html',
+			templateUrl: 'app/components/home/view.html',
 			controller: 'homeController'
 		})
 		.when('/chat', {
-			templateUrl: '/app/components/chat/view.html',
+			templateUrl: 'app/components/chat/view.html',
 			controller: 'chatController'
 		})
 		.when('/shop', {
-			templateUrl: '/app/components/shop/view.html',
+			templateUrl: 'app/components/shop/view.html',
 			controller: 'shopController'
 		})
-		.when('shop/:itemId', {
-			templateUrl: '/app/components/shop/item.html',
+		.when('/shop/:itemId', {
+			templateUrl: 'app/components/shop/item.html',
 			controller: 'shopItemController'
 		})
 		.when('/tickets', {
-			templateUrl: '/app/components/tickets/view.html',
+			templateUrl: 'app/components/tickets/view.html',
 			controller: 'ticketsController'
 		})
 		.otherwise({
@@ -36,14 +36,15 @@ ngApp.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 ngApp.controller('mainController', ['$scope', function($scope, $sce){
+	$scope.dom_loading = 1;
 	$scope.include_loading = 1;
 	$scope.route_loading = 0;
 	$scope.client = 'open kinetix';
 	$scope.currentDateObject = new Date();
 
-	/*angular.element(document).ready(function () {
-        $scope.app_loading = 0;
-    });*/
+	angular.element(document).ready(function () {
+		$scope.dom_loading = 0;
+    });
 
 	$scope.$on('$includeContentRequested', function(event) {
 		$scope.include_loading = 1;
