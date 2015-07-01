074046fc2377b1ace30a9cfcdca18bcfa65c3152
Arnold J. Rimmer <djordjekg@gmail.com>
2015-07-01 19:04:42 +0200

    tt

--- client/www/app/shared/radialmenu/controller.js
+++ client/www/app/shared/radialmenu/controller.js
@@ -2,10 +2,13 @@
 ngApp.controller('radialmenuController', ['$scope', function($scope, $sce){
 	$scope.$on('$includeContentLoaded', function(event) {
 		var el = angular.element, sel = document.querySelector;
+		var holder = angular.element(document.querySelector('#radialmenu'));
+		var bd = angular.element(document.querySelector('#navs_backdrop'));
 		var ul = angular.element(document.querySelector('#navs')), li = ul.children(), i = li.length, n = i - 1, r = 100;
 		ul.bind('click', function() {
-			angular.element(this).toggleClass('active');
-			if (angular.element(this).hasClass('active')){
+			//angular.element(this).toggleClass('active');
+			holder.toggleClass('active');
+			if (holder.hasClass('active')){
 				for (var a = 0; a < i; a++){
 					li.eq(a).css({
 						'transition-delay': '' + (40 * a) + 'ms',
@@ -17,6 +20,10 @@ ngApp.controller('radialmenuController', ['$scope', function($scope, $sce){
 			}else{
 				li.removeAttr('style');
 			}
+		});
+		bd.bind('click', function() {
+			holder.toggleClass('active');
+			li.removeAttr('style');
 		});
     });
 }]);
