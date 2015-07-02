ac281641f01ba8a09357c43aa455ec44d50074ce
Igor Kostic <igorkg@gmail.com>
2015-07-02 11:28:50 +0200

    Chat component implemented

--- client/www/app/components/chat/controller.js
+++ client/www/app/components/chat/controller.js
@@ -1,9 +1,11 @@
 
 ngApp.controller('chatController', ['$scope', 'socket', function($scope, socket){
   $scope.sendMessage = function() {
-    socket.emit('message', $scope.userName);
+    socket.emit('message', $scope.chatMessage);
+    $scope.chatMessage = '';
   }
   $scope.$on('socket:broadcast', function(event, data) {
     console.log(data);
+    angular.element( document.querySelector( '#messages' ) ).append('<li>' + data + '</li>')
   });
 }]);
--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ var io         = require('socket.io')(http);
 
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
+
+//var socket     = require('./lib/socket.js');
 //var meetupsController = require('./controllers/meetups-controller');
 
 app.use(bodyParser.json())
+
+//io.sockets.on('connection', socket);
 
 http.listen(3000, function() {
   console.log('I\'m Listening...');
@@ -15,7 +19,7 @@ http.listen(3000, function() {
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  var msg = 'hello';
+  var msg = 'Hi, Welcome to Chat';
 
   socket.emit('broadcast', msg);
 
